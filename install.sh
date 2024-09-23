bda06e361a2fac1a0374cfd2f7218742059778de
xiajihnu <xiajihnu@163.com>
2024-09-23 09:45:43 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#=================================================================#
+#   System Required:  CentOS 6+, Debian 7+, Ubuntu 12+            #
+#   Description: One click Install home-nas server                #
+#   Author: Xia Ji                                                #
+#=================================================================#
+
+clear
+echo
+echo "#############################################################"
+echo "# One click Install home-nas server                         #"
+echo "# Author: Xia Ji                                            #"
+echo "#############################################################"
+echo
